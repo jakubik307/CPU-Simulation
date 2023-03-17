4b655dab33a19d86b1038a9c52d38125f00fd093
jakubik307 <and.jj.an@gmail.com>
2023-03-17 23:23:07 +0100

    Add RR algorithm and pick up time measurin

--- FCFS.java
+++ FCFS.java
@@ -3,21 +3,26 @@ import java.util.ArrayList;
 public class FCFS extends Algorithm {
     @Override
     public void startSimulation(ArrayList<Request> requests) {
+
+        initialState();
         copyQueue(requests);
 
         while (finishedRequests < Main.simulationSize) {
-
             addToActiveQueue();
 
             //Add request from queue to processing
             if (activeQueue.size() != 0 && activeRequest == null) {
                 activeRequest = activeQueue.get(0);
-                activeQueue.remove(activeQueue.get(0));
+                activeQueue.remove(activeRequest);
                 requestSwaps++;
             }
 
             //Processing active request
-            else if (activeRequest != null) {
+            if (activeRequest != null) {
+                if (activeRequest.getPickUpTime() == 0) {
+                    activeRequest.setPickUpTime(currentTime - activeRequest.getArrivalTime());
+                }
+
                 activeRequest.setTimeToComplete(activeRequest.getTimeToComplete() - 1);
 
                 if (activeRequest.getTimeToComplete() == 0) {
--- RR.java
+++ RR.java
@@ -1,2 +1,67 @@
-public class RR {
+import java.util.ArrayList;
+
+public class RR extends Algorithm {
+    private int timeInterval;
+    private int previousRefreshTime = 0;
+
+    @Override
+    public void startSimulation(ArrayList<Request> requests) {
+
+        initialState();
+        copyQueue(requests);
+
+        while (finishedRequests < Main.simulationSize) {
+            int cycleTime = 0;
+
+            addToActiveQueue();
+            previousRefreshTime = currentTime;
+
+            if (activeQueue.size() > 0) {
+
+                requestSwaps += activeQueue.size();
+
+                for (int i = 0; i < activeQueue.size(); i++) {
+
+                    activeRequest = activeQueue.get(i);
+                    activeRequest.setWaitingTime(activeRequest.getWaitingTime() + cycleTime);
+
+                    if (activeRequest.getPickUpTime() == 0) {
+                        activeRequest.setPickUpTime(currentTime - activeRequest.getArrivalTime() + cycleTime);
+                    }
+
+                    cycleTime += Math.min(activeRequest.getTimeToComplete(), timeInterval);
+
+                    if (activeRequest.getTimeToComplete() <= timeInterval) {
+                        //Process is going to finish
+                        activeRequest.setTimeToComplete(0);
+                        activeQueue.remove(i);
+                        i--;
+                        finishedRequests++;
+                    } else {
+                        //Process needs to take more rounds
+                        activeRequest.setTimeToComplete(activeRequest.getTimeToComplete() - timeInterval);
+                    }
+                }
+            }
+            currentTime += Math.max(cycleTime, 1);
+        }
+
+        System.out.println("RR");
+        printResults();
+        System.out.println("----------");
+    }
+
+    @Override
+    protected void addToActiveQueue() {
+        for (Request request : simulationQueue) {
+            if (request.getArrivalTime() > previousRefreshTime && request.getArrivalTime() <= currentTime) {
+                activeQueue.add(request);
+                request.setWaitingTime(currentTime - request.getArrivalTime());
+            }
+        }
+    }
+
+    public void setTimeInterval(int timeInterval) {
+        this.timeInterval = timeInterval;
+    }
 }
